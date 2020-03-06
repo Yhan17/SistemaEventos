8ccf8fc796c076495d695ae471de119f309cb58c
Yhan17 <shikamaru.nara897@gmail.com>
2020-03-06 19:27:11 -0300

    Começo Front end

---
+++ src/br/unitins/eventos/controller/IndexController.java
@@ -0,0 +1,13 @@
+package br.unitins.eventos.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+@RequestScoped
+@Named("indexController")
+public class IndexController implements Serializable{
+
+	private static final long serialVersionUID = 8830256567017040262L;
+
+}
